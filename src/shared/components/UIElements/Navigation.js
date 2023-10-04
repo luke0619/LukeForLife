5a346fec38c10496de5ab708136d06823a23be18
luke0619s <sunluke0619@gmail.com>
2023-10-04 08:12:04 +0800

    更改主選單標題

--- src/shared/components/UIElements/Navigation.js
+++ src/shared/components/UIElements/Navigation.js
@@ -5,7 +5,7 @@ function Navigation() {
         <ul className="navigation__list">
             <li><NavLink className="navigation__item" to={"/"}>首頁</NavLink></li>
             <li><NavLink className="navigation__item" to={"/resume"}>履歷表</NavLink></li>
-            <li><a className="navigation__item" href="https://auth-8aa20.web.app">網站作品集</a></li>
+            <li><a className="navigation__item" href="https://auth-8aa20.web.app">網站作品</a></li>
         </ul>
     )
 }
